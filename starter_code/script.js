6a7d0221f59d59035666d94ad902779f4e5c0912
jpprieto <misticientist@gmail.com>
2016-11-15 23:22:22 +0000

    :3

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,15 +1,15 @@
 // Task 1: Create a function that randomly chooses among 'rock', 'paper', or 'scissors'.
 var generateComputerChoice = function () {
     // Task 1, Step 1: Create an array with three elements ("rock", "paper" and "scissors").
-        
+        var rps = ["rock", "paper", "scissors"];
     // Task 1, Step 2: Use the JavaScript Math function to generate a random whole
     // number between 0 and 2. Be sure to save it to a variable.
-        
+        var rand = (Math.floor(Math.random()*3));
     // Task 1, Step 3: Use this randomly generated number to pull a value from the
     // array (eg myArray[randomNum])
-        
+        console.log(rps[rand]);
     // Task 1, Step 4: return this new value
-        
+        return rps[rand];
 };
 
 // Task 2: Create a function that compares the userChoice and the computerChoice
@@ -19,7 +19,15 @@ var pickWinner = function (userChoice, computerChoice) {
     console.log("computer choice: " + computerChoice);
     // Task 2, Step 1: Create an "if/else if/else" statement that compares the
     // userChoice and computerChoice under any possible game outcome.
-       
+       if(userChoice=rock && computerChoice=scissors){
+           
+       } else if(userChoice=paper && computerChoice=rock){
+           
+       } else if(userChoice=scissors && computerChoice=paper){
+           
+       } else if(userChoice=scissors && computerChoice=rock){
+           
+       } else if(userChoice=)
     // Task 2, Step 2: Depending on who is the winner of the game console.log
     // either "user wins", "computer wins" or "draw"
 
